0c4537bca91de4a3f0bf866cefb2639579023ba6
Priyanshu Parashar <priyanshuparashar06@gmail.com>
2024-04-06 20:14:12 +0530

    Variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2310023
+let accountEmail = 'sujeetgund@gmail.com'
+var accountPassword = 2732
+accountCity = 'Bhopal'
+let accountState;
+
+// accountId = 3000
+accountEmail = 'sujeet@dev.com'
+accountPassword = 23454
+accountCity = 'Barshi'
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
